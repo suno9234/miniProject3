1f00c9b8cb9afd32f220eb489b5fa48dc1236982
suminnnnn <tlsssm1212@ajou.ac.kr>
2025-10-29 02:40:05 +0900

    [feat] 메인 그래프 정의

--- backend/app/nodes/graph.py
+++ backend/app/nodes/graph.py
@@ -3,8 +3,8 @@ from app.nodes.state import AppState
 from app.nodes.planner import planner_node
 
 # TODO: 각 에이전트 노드 임포트 추가
-# from app.nodes.search_agent.response_generator import search_agent_graph
-# from app.nodes.inventory_agent.inventory_node import inventory_agent_node
+from app.nodes.search_agent.graph import search_agent_graph
+from app.nodes.inventory_agent.graph import inventory_agent_graph
 # from app.nodes.reservation_agent.reservation_node import reservation_agent_node
 
 def route_to_agent(state: AppState) -> str:
@@ -26,8 +26,8 @@ def build_graph():
     graph.add_node("planner", planner_node)
     
     # TODO: 각 에이전트 노드 추가
-    # graph.add_node("search_agent", search_agent_graph)
-    # graph.add_node("inventory_agent", inventory_agent_node)
+    graph.add_node("search_agent", search_agent_graph)
+    graph.add_node("inventory_agent", inventory_agent_graph)
     # graph.add_node("reservation_agent", reservation_agent_node)
 
     # 엣지 연결
@@ -38,13 +38,13 @@ def build_graph():
         {
             "search_agent": "search_agent",
             "inventory_agent": "inventory_agent", 
-            "reservation_agent": "reservation_agent"
+            #"reservation_agent": "reservation_agent"
         }
     )
     
     # TODO: 각 에이전트에서 END로 연결
-    # graph.add_edge("search_agent", END)
-    # graph.add_edge("inventory_agent", END)
+    graph.add_edge("search_agent", END)
+    graph.add_edge("inventory_agent", END)
     # graph.add_edge("reservation_agent", END)
 
     return graph.compile()
