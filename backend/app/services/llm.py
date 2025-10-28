0f84afb8d20d1a08a7782d49ce7dce8af5bc7660
ljaehwi <ljaehwi57@gmail.com>
2025-10-28 14:32:34 +0900

    모델 선택 가능

--- backend/app/services/llm.py
+++ backend/app/services/llm.py
@@ -3,11 +3,34 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from app.models import AgentRoute
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
+import torch
 
 load_dotenv()
 
-INTERNAL_LLM_MODEL = os.getenv(INTERNAL_LLM_MODEL,"qwen/Qwen1.5-0.5B-Chat")
+HF_TOKEN = os.getenv("HF_TOKEN")
+ENVIRONMENT = os.getenv("ENVIRONMENT", "test")
+
+if ENVIRONMENT == "operate":
+    # In production, use a different model as needed.
+    INTERNAL_LLM_MODEL = "meta-llama/Meta-Llama-3-8B-Instruct"
+    device_map = "cuda"
+    
+    quantization_config = BitsAndBytesConfig(load_in_4bit=True)
+    
+    tokenizer = AutoTokenizer.from_pretrained(INTERNAL_LLM_MODEL, token=HF_TOKEN)
+    internal_llm = AutoModelForCausalLM.from_pretrained(
+        INTERNAL_LLM_MODEL,
+        device_map=device_map,
+        quantization_config=quantization_config,
+        token=HF_TOKEN
+    )
+else:
+    INTERNAL_LLM_MODEL = "qwen/Qwen1.5-0.5B-Chat"
+    device_map = "auto"
+    tokenizer = AutoTokenizer.from_pretrained(INTERNAL_LLM_MODEL)
+    internal_llm = AutoModelForCausalLM.from_pretrained(INTERNAL_LLM_MODEL, device_map=device_map)
+
 
 # 기본 LLM
 llm = ChatOpenAI(
@@ -16,8 +39,7 @@ llm = ChatOpenAI(
     api_key=os.getenv('OPENAI_API_KEY')
 )
 
-tokenizer = AutoTokenizer.from_pretrained(INTERNAL_LLM_MODEL)
-internal_llm = AutoModelForCausalLM.from_pretrained(INTERNAL_LLM_MODEL, device_map="auto")
+
 
 internal_pipe = pipeline("text-generation", model=internal_llm, tokenizer=tokenizer, max_new_tokens=256)
 
